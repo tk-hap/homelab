16a7a7b6356377f32944e9d04d4ce02b9944564e
Te Kaanu Hapuku <34885571+tk-hap@users.noreply.github.com>
2025-06-20 16:11:09 +1200

    terraform fmt

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -13,24 +13,24 @@ module "talos" {
 
   nodes = {
     "control-01" = {
-      ip           = "192.168.0.205"
-      cpu          = 2
-      ram          = 12000
-      machine_type = "controlplane"
+      ip            = "192.168.0.205"
+      cpu           = 2
+      ram           = 12000
+      machine_type  = "controlplane"
       image_version = "v1.10.1"
     }
     "control-02" = {
-      ip           = "192.168.0.206"
-      cpu          = 2
-      ram          = 12000
-      machine_type = "controlplane"
+      ip            = "192.168.0.206"
+      cpu           = 2
+      ram           = 12000
+      machine_type  = "controlplane"
       image_version = "v1.10.1"
     }
     "control-03" = {
-      ip           = "192.168.0.207"
-      cpu          = 2
-      ram          = 12000
-      machine_type = "controlplane"
+      ip            = "192.168.0.207"
+      cpu           = 2
+      ram           = 12000
+      machine_type  = "controlplane"
       image_version = "v1.10.1"
     }
   }
